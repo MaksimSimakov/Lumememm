bcb203271b2ffdcd9d3110e99b96b96eae031a5d
Opilane TTHK <opilane@tthk.ee>
2020-09-18 13:17:43 +0300

    Add project files.

---
+++ Lumememm/Lumememm/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lumememm
+{
+    public partial class MainPage : ContentPage
+    { bool vkl;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void on_Clicked(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
